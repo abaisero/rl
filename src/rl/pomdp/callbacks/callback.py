489395487c53113262b85a1a3b1a1f0bf7a1cc24
abaisero <andrea.baisero@gmail.com>
2018-03-02 16:13:35 -0500

    remove poor plotting attempts

--- src/rl/pomdp/callbacks/callback.py
+++
@@ -1,9 +0,0 @@
-class Callback:
-    def __init__(self, env):
-        self.env = env
-
-    def feedback(self, sys, context, a, feedback):
-        pass
-
-    def feedback_episode(self, sys, episode):
-        pass
